66e9f011c88d13f38857e4d434a84884897b08d1
Dani Dominguez <dani.d.zamorano@gmail.com>
2021-10-25 22:42:21 +0200

    Improve button component

--- components/button/index.js
+++ components/button/index.js
@@ -1,5 +1,33 @@
+import clsx from 'clsx'
 import s from './button.module.css'
 
-export default function Button({ children }) {
-  return <button className={s.button}>{children}</button>
+export default function Button({
+  className,
+  variant = 'primary',
+  active,
+  disabled,
+  children,
+  ...rest
+}) {
+  const rootClassName = clsx(
+    s.root,
+    {
+      [s.primary]: variant === 'primary',
+      [s.secondary]: variant === 'secondary',
+      [s.tertiary]: variant === 'tertiary',
+    },
+    className
+  )
+
+  return (
+    <button
+      aria-pressed={active}
+      data-variant={variant}
+      className={rootClassName}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
 }
