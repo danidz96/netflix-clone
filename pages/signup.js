87e3ca0d62943e35ee4a6d48e4ffd981f3525ab4
Dani Dominguez <dani.d.zamorano@gmail.com>
2021-11-20 16:39:51 +0100

    Add sign up logic

--- pages/signup.js
+++ pages/signup.js
@@ -1,18 +1,77 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Form from '@components/form'
 import HomepageBackground from '@components/homepageBackground'
+import { useUser } from '@hooks/useUser'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState({
+    type: '',
+    content: '',
+  })
+
+  const router = useRouter()
+  const { signUp, user } = useUser()
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+
+    setLoading(true)
+    setMessage({})
+    const { error, user: createdUser } = await signUp({ email, password })
+    if (error) {
+      setMessage({ type: 'error', content: error.message })
+    } else {
+      if (createdUser) {
+        setMessage({
+          type: 'note',
+          content: 'Check your email for the confirmation link.',
+        })
+      }
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/browse')
+    }
+  }, [user])
+
   return (
     <HomepageBackground>
       <div className="h-screen flex items-center">
         <Form.Container>
-          <Form>
+          <Form onSubmit={handleSignup}>
             <Form.Title>Sign Up</Form.Title>
-            <Form.Input placeholder="Name" />
-            <Form.Input type="email" placeholder="Email" />
-            <Form.Input type="password" placeholder="Password" />
-            <Form.Button type="submit">Sign Up</Form.Button>
+            {message.content && (
+              <div
+                className={`${
+                  message.type === 'error' ? 'text-red-600' : 'text-white'
+                }`}
+              >
+                {message.content}
+              </div>
+            )}
+            <Form.Input placeholder="Name" onChange={setName} />
+            <Form.Input type="email" placeholder="Email" onChange={setEmail} />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              onChange={setPassword}
+            />
+            <Form.Button
+              type="submit"
+              // loading={loading}
+              disabled={loading || !email.length || !password.length}
+            >
+              Sign Up
+            </Form.Button>
             <span className="pt-1 text-center text-sm text-white">
               <span>Do you have an account?</span>
               {` `}
